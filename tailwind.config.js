8750d7e7757cfb5569b8cb57ce544bbd1866289b
Denzel Elden Wijaya <137489821+denzel-afk@users.noreply.github.com>
2023-12-18 19:07:01 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary:'#FF6363',
+        secondary:{
+          100: '#e2e2d5',
+          200: '#888883',
+        }
+      },
+      fontFamily:{
+        body:['Nunito']
+      }
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
+
